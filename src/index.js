29174274f556c674206ad2ea313caf3ec8a2f96d
Alextufeng <salman-aka@mail.ru>
2022-02-24 23:17:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let newArray = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    newArray.push(bracketsConfig[i].join(''));
+  }
+
+  for (let j = 0; j < newArray.length; j++) { 
+    if (str.includes(newArray[j])) { 
+      str = str.replace(newArray[j], '');
+      j = -1;
+    }
+  }
+
+  if (str.length === 0) {
+    return true
+  } else { 
+    return false;
+  }
+
 }
